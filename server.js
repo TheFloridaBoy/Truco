c757f240fbdbb28fbff8a5a4f1ee06d7e9095757
TheFloridaBoy <suarezpedrove@yahoo.com>
2020-08-03 21:54:39 -0400

    m

--- server.js
+++ server.js
@@ -123,7 +123,9 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(3000, function () {
+//http.listen(3000, function () {
+
+http.listen(process.env.PORT || 3000, function () {
     console.log('Server started!');
 });
 
